d46db61b3a8ca7b5b4cf2413e2d6770177c48386
Alexander Huber <alih83@gmx.de>
2020-02-10 07:26:02 +0100

    fix pretier/eslint

--- server/main.js
+++ server/main.js
@@ -3,10 +3,10 @@ import { onPageLoad } from 'meteor/server-render';
 
 Meteor.startup(() => {
   // Code to run on server startup.
-  console.log(`Greetings from ${module.id}!`);
+  // console.log(`Greetings from ${module.id}!`);
 });
 
-onPageLoad(sink => {
+onPageLoad((sink) => {
   // Code to run on every request.
   // sink.renderIntoElementById(
   //   "server-render-target",
