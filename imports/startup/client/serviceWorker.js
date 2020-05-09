e32771bc42d4576acadfc47f082cac589184bed5
Alexander Huber <alih83@gmx.de>
2020-05-09 17:13:06 +0200

    fix serviceWorker not defined

--- imports/startup/client/serviceWorker.js
+++ imports/startup/client/serviceWorker.js
@@ -1,10 +1,12 @@
 import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
-  navigator.serviceWorker
-    .register('/sw.js')
-    .then(() => console.info('service worker registered'))
-    .catch((error) => {
-      console.log('ServiceWorker registration failed: ', error);
-    });
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then(() => console.info('service worker registered'))
+      .catch((error) => {
+        console.log('ServiceWorker registration failed: ', error);
+      });
+  }
 });
