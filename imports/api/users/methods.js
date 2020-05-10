0fbfa4f2e40d43fb0fc54a0f015b98dcd5dcd71c
Alexander Huber <alih83@gmx.de>
2020-05-10 15:42:40 +0200

    add winston logging

--- imports/api/users/methods.js
+++ imports/api/users/methods.js
@@ -1,20 +1,33 @@
 import { Meteor } from 'meteor/meteor';
 import { Match, check } from 'meteor/check';
 import { Accounts } from 'meteor/accounts-base';
-// import { logger }       from '../../server-logger';
+
+const { createLogger, transports, format } = require('winston');
+
+const { combine, timestamp, label, printf } = format;
+
+const loggerFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}] ${level}: ${message}`;
+});
+
+const logger = createLogger({
+  format: combine(label({ label: 'api/users/methods' }), timestamp(), loggerFormat),
+  transports: [new transports.Console()],
+});
+
 
 Meteor.methods({
-  removeUser: function(userId) {
+  removeUser: function (userId) {
     check(userId, String);
     const isAdmin = Meteor.users.findOne(this.userId).admin;
     if (isAdmin) {
-      // logger.warn(`deleted user with id ${userId}`);
+      logger.info(`deleted user with id ${userId}`);
       Meteor.users.remove({ _id: userId });
     } else {
       throw new Meteor.Error('delete user error');
     }
   },
-  updateUser: function(data) {
+  updateUser: function (data) {
     const pattern = {
       _id: String,
       username: String,
@@ -29,12 +42,14 @@ Meteor.methods({
         Accounts.setPassword(data._id, data.password);
       }
       Accounts.setUsername(data._id, data.username);
-      Meteor.users.update({ _id: data._id }, { $set: { admin: data.admin } });
+      const adminSet = data.admin || false;
+      Meteor.users.update({ _id: data._id }, { $set: { admin: adminSet } });
+      logger.info(`updated user with id ${data._id}, set ADMIN to ${adminSet}`);
     } else {
       throw new Meteor.Error('update user error');
     }
   },
-  addUser: function(data) {
+  addUser: function (data) {
     const pattern = {
       username: String,
       password: String,
@@ -47,6 +62,7 @@ Meteor.methods({
       const newId = Accounts.createUser(data);
       const admin = data.admin || false;
       Meteor.users.update({ _id: newId }, { $set: { admin } });
+      logger.info(`created user with id ${newId}, set ADMIN to ${admin}`);
     } else {
       throw new Meteor.Error('add user error');
     }
--- server/main.js
+++ server/main.js
@@ -5,17 +5,30 @@ import './agenda';
 import './publications.js';
 import '../imports/startup/server/methods';
 
+const { createLogger, transports, format } = require('winston');
+
+const { combine, timestamp, label, printf } = format;
+
+const loggerFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}] ${level}: ${message}`;
+});
+
+const logger = createLogger({
+  format: combine(label({ label: 'server/main' }), timestamp(), loggerFormat),
+  transports: [new transports.Console()],
+});
+
 Meteor.startup(() => {
   const user = Meteor.users.findOne({ username: 'admin' });
   if (!user) {
-    // logger.info('admin user not found, seeding admin user...');
+    logger.info('admin user not found, seeding admin user...');
     Meteor.users.insert({ username: 'admin', admin: true });
     const newUser = Meteor.users.findOne({ username: 'admin' });
-    const pw = process.env.ADMIN;
+    const pw = process.env.ADMIN || 'adminadmin';
     Accounts.setPassword(newUser._id, pw);
   }
 
-  // logger.info(`server started... registered users: ${Meteor.users.find({}).fetch().length}`);
+  logger.info(`\nserver started... registered users: ${Meteor.users.find({}).fetch().length}`);
 });
 
 onPageLoad((sink) => {
